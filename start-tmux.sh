f57176e9bd58e22664af92573ca66a74c56e3c06
Ezra Godfrey <ezra.godfrey@gmail.com>
2019-12-26 14:57:00 -0700

    tmux: updated the start-tmux.sh script
    Added a vertical split to the second window created in the script.
    Added a start directory and renamed the third window created in the
    script.

--- start-tmux.sh
+++ start-tmux.sh
@@ -6,8 +6,8 @@ tmux has-session -t $SESSION_NAME &> /dev/null
 if [ $? != 0 ]
  then
     tmux new-session -s $SESSION_NAME -n bld -d -P
-    tmux new-window -n src -d -P
-    tmux new-window -n misc -d -P
+    tmux new-window -n src -P \; split-window -h
+    tmux new-window -n video -P -c /mnt/NAS/video
 fi
 
 tmux attach -t $SESSION_NAME
