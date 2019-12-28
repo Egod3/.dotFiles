c5acf8d84ea8ed8cc6dde86e8d6cdc83fba9f100
Ezra Godfrey <ezra.godfrey@gmail.com>
2019-12-28 07:44:38 -0700

    tmux: change tmux windows to start in ~/.dotFiles

--- start-tmux.sh
+++ start-tmux.sh
@@ -5,8 +5,11 @@ tmux has-session -t $SESSION_NAME &> /dev/null
 
 if [ $? != 0 ]
  then
-    tmux new-session -s $SESSION_NAME -n bld -d -P
-    tmux new-window -n src -P \; split-window -h
+    tmux new-session -s $SESSION_NAME -n bld -d -c ~/.dotFiles -P
+    tmux new-window -n src -c ~/.dotFiles -P \; split-window -h -c ~/.dotFiles
+#   Use the next two commands to create 4 equal sized panes in the src window
+#    tmux new-window -n src -c ~/.dotFiles -P \; split-window -h -c ~/.dotFiles \; split-window -v -c ~/.dotFiles
+#    tmux select-pane -t:.0 \; split-window -v -c ~/.dotFiles
     tmux new-window -n video -P -c /mnt/NAS/video
 fi
 
