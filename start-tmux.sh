59668f6810806d055858ec15d3ef4cc388db060e
ezra <Ezra J Godfrey>
2022-12-29 12:36:48 -0700

    start-tmux: updated window names and paths

--- start-tmux.sh
+++ start-tmux.sh
@@ -14,32 +14,34 @@ tmux has-session -t $SESSION_NAME &> /dev/null
 if [ $? != 0 ]
   then
   # 0
-  tmux -2v new-session -s $SESSION_NAME -n bld -d -c  ~/.dotFiles
+  tmux -2v new-session -s $SESSION_NAME -n dotFiles -d -c  ~/.dotFiles
   tmux split-window -h -c ~/.dotFiles
 
   # 1
-  tmux new-window -n video -c /mnt/NAS/video
-
-  # 2
   tmux new-window -n png_sekrt -c /mnt/NAS/data/git/learning/png_sekrt
   tmux split-window -h -c /mnt/NAS/data/git/learning/png_sekrt/src
 
-  # 3
+  # 2
 #  tmux new-window -n rename_media -c /mnt/NAS/data/git/tools/rename_media
 #  tmux split-window -h -c /mnt/NAS/data/git/tools/rename_media/src
 
-  # 3
+  # 2
   tmux new-window -n stm32 -c /mnt/NAS/data/git/rust_embd/apps/stm32-rs
   tmux split-window -h -c /mnt/NAS/data/git/rust_embd/apps/stm32-rs
   tmux selectp -t 0
   tmux split-window -v -c /mnt/NAS/data/git/rust_embd/apps/stm32-rs
 
-  # 4
+  # 3
   tmux new-window -n si70xx -c /mnt/NAS/data/git/rust_embd/libs/si70xx
   tmux split-window -h -c /mnt/NAS/data/git/rust_embd/libs/si70xx
 
+  # 4
+  tmux new-window -n tockOS -c /mnt/NAS/data/git/rust_embd/oses/tock
+  tmux split-window -h -c /mnt/NAS/data/git/rust_embd/oses/tock
+
   # 5
-  tmux new-window -n calcualtor -c ~/
+  tmux new-window -n video -c /mnt/NAS/video
+
 fi
 
 tmux attach -t $SESSION_NAME
