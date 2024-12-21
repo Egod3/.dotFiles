350a7ce05879958b73832e45cdc9c7028feb6c81
Ezra J Godfrey <ezra.godfrey@gmail.com>
2024-12-21 07:18:23 -0700

    add restore function

--- backup_nas.sh
+++ backup_nas.sh
@@ -1,8 +1,47 @@
 #!/usr/bin/bash
 
+# Restore function that is the inverse of backup_nas_other
+function restore_nas_other(){
+  local restore_data=$1
+  local restore_music=$2
+  local restore_photo=$3
+  local restore_path="/mnt/backup/archive/2024"
+  local restore_log="restore_$(date +'%m_%d_%Y_%H_%M').log"
+  local data_path="/volume1/data"
+  local music_path="/volume1/music"
+  local photo_path="/volume1/photo"
+  if [ -d $restore_path ]; then
+    if [ -d $data_path ]; then
+      cd $data_path
+      echo "Restoring file $restore_data with tar" 2>&1 | tee $restore_log
+      time tar -vxf $restore_path/$restore_data 2>&1 | tee -a $restore_log
+    else
+      echo "$data_path not found"
+    fi
+
+    if [ -d $music_path ]; then
+      cd $music_path
+      echo "Restoring file $restore_music with tar" 2>&1 | tee $restore_log
+      time tar -vxf $restore_path/$restore_music 2>&1 | tee -a $restore_log
+    else
+      echo "$music_path not found"
+    fi
+
+    if [ -d $photo_path ]; then
+      cd $photo_path
+      echo "Restoring file $restore_photo with tar" 2>&1 | tee $restore_log
+      time tar -vxf $restore_path/$restore_photo 2>&1 | tee -a $restore_log
+    else
+      echo "$photo_path not found"
+    fi
+  else
+    echo "couldn't find restore $restore_path directory, bailing"
+  fi
+}
+
 # Check the space required and ensure you have enough space
-# before starting the rsync
-function backup_nas_data_music_photo(){
+# before starting the tar's
+function backup_nas_other(){
   local backup_path="/media/$(whoami)/nas_backup/archive"
   local backup_log="backup_$(date +'%m_%d_%Y_%H_%M').log"
   local data_path="/mnt/nas/data"
@@ -80,8 +119,9 @@ function backup_nas_movies(){
 usage() {
   echo "Usage: $0
          [-d backup data/music/photo (~0.5TB)]
-         [-t backup video/tv shows (~5.0TB)]
-         [-m backup video/movies (~4.0TB)]" 1>&2; exit 1;
+         [-t backup video/tv shows (~5.5TB)]
+         [-m backup video/movies (~4.4TB)]" 1>&2;
+  exit 1;
 }
 
 while getopts ":dtm" o; do
@@ -107,7 +147,7 @@ if [ -z "${d}" ] && [ -z "${t}" ] && [ -z "${m}" ]; then
 fi
 
 if [ ${d} ]; then
-  backup_nas_data_music_photo
+  backup_nas_other
 fi
 if [ ${t} ]; then
   backup_nas_tv
