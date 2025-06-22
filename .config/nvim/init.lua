f5e0ea11c2751d7987a5e39cb76bf56acb699558
Ezra J Godfrey <ezra.godfrey@gmail.com>
2025-06-22 11:30:26 -0600

    init.lua: remove mason func that caused issues
    setup-dotfiles: Update install list.
    setup-tmux-ezra-lnx: update comments and cleanup.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -511,15 +511,15 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
--- mason_lspconfig.setup_handlers {
---   function(server_name)
---     require('lspconfig')[server_name].setup {
---       capabilities = capabilities,
---       on_attach = on_attach,
---       settings = servers[server_name],
---     }
---   end,
--- }
+vim.lsp.config('ruff', {
+  init_options = {
+    settings = {
+      -- Ruff language server settings go here
+    }
+  }
+})
+
+vim.lsp.enable('ruff')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- start-tmux-ezra-lnx.sh
+++ start-tmux-ezra-lnx.sh
@@ -18,38 +18,30 @@ if [ $? != 0 ]
   tmux -2 new-session -s $SESSION_NAME -n dotFiles -d -c    ~/.dotFiles
   tmux split-window -h -c                                   ~/.dotFiles
   # 1
-  #tmux new-window -n stm32l4hal -c                          ~/git/rust_embd/libs/stm32l4xx-hal/examples
-  #tmux split-window -h -c                                   ~/git/rust_embd/libs/stm32l4xx-hal/examples
-  # 2
   tmux new-window -n stm32 -c                               ~/git/stm32-rs
   tmux split-window -h -c                                   ~/git/stm32-rs
   tmux selectp -t 0
   tmux split-window -v -c                                   ~/git/stm32-rs
   tmux split-window -v -c                                   ~/git/stm32-rs
-  #tmux new-window -n uart-stm32 -c                          ~/git/uart-stm32
-  #tmux split-window -h -c                                   ~/git/uart-stm32
-  #tmux new-window -n si70xx -c                              ~/git/si70xx
-  #tmux split-window -h -c                                   ~/git/si70xx
-  ### Remember to run this command in the below windows - $ setup_west ###
-  # 3
+  # 2
   tmux new-window -n zephyr-rust -c                         ~/git/ws-app
   tmux split-window -h -c                                   ~/git/ws-app
   tmux selectp -t 0
   tmux split-window -v -c                                   ~/git/ws-app
-  # 4
+  # 3
   tmux new-window -n hexdump -c                             ~/git/tools/hd
   tmux split-window -h -c                                   ~/git/tools/hd
   tmux selectp -t 0
   tmux split-window -v -c                                   ~/git/tools/hd
-  # 5
+  # 4
   tmux new-window -n loot_table -c                          ~/git/tools/loot_table
   tmux split-window -h        -c                            ~/git/tools/loot_table
   tmux selectp -t 0
   tmux split-window -v        -c                            ~/git/tools/loot_table
-  # 6
+  # 5
   tmux new-window -n mcuboot-rs -c                          ~/git/mcuboot-rs
   tmux split-window -h -c                                   ~/git/mcuboot-rs
-  # 7
+  # 6
   tmux new-window -n bash -c                                ~
 
 fi
