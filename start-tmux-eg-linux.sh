ee4e65e0489cc9d66280911564756f7c49aa66f6
Ezra Godfrey <eg@fieldlineinc.com>
2026-01-16 11:22:47 -0700

    start-tmux-eg-linux: remove fli workspace
    Update to the latest tmux workspace layout.

--- start-tmux-eg-linux.sh
+++ start-tmux-eg-linux.sh
@@ -5,7 +5,7 @@ if [[ $1 != "" ]]
 then
   SESSION_NAME=$1
 else
-  SESSION_NAME="fli"
+  SESSION_NAME="flm"
 fi
 tmux has-session -t $SESSION_NAME &> /dev/null
 ret=$?
@@ -13,12 +13,9 @@ ret=$?
 echo "has-session returned: $ret"
 echo "Attempting to create/attach to session $SESSION_NAME"
 
-SCALAR_ROOT=/workspace/fli-scalar/fli-scalar_0
-FLI_ROOT=/workspace/fli
-NQMA_ROOT=$FLI_ROOT/nqma
 FLM_ROOT=/workspace/flm
 HEDSCAN_ROOT=$FLM_ROOT/hedscan
-PYTHON_VENV_PATH=~/.venv/bin/activate
+PYTHON_VENV_PATH=~/.venv-uv/bin/activate
 
 pushd /workspace
 
@@ -26,65 +23,7 @@ if [ $ret -eq 1 ]; then
   # 0
   tmux -2 new -s $SESSION_NAME -n workspace -d -c /workspace
   tmux split-window -h                         -c /workspace
-  if [[ "$SESSION_NAME" == "fli" ]]; then
-    # 0
-    tmux new-window -n dotFiles                -c ~/.dotFiles
-    tmux split-window -h                       -c ~/.dotFiles
-    # 1
-    tmux new-window -n notes                   -c /workspace/notes
-    tmux split-window -h                       -c /workspace/notes
-    # 2
-    ### Remember to run this command in the below windows - $ setup_west ###
-    tmux new-window -n zephyr                  -c $SCALAR_ROOT/cxp-zephyr-scalar-mz
-    tmux split-window -h                       -c $SCALAR_ROOT/cxp-zephyr-scalar-mz
-    tmux split-window -v                       -c $SCALAR_ROOT/cxp-zephyr-scalar-mz/boards
-    tmux selectp -t 0
-    tmux split-window -v                       -c $SCALAR_ROOT/cxp-zephyr-scalar-mz/boards
-    # 3
-    tmux new-window -n yocto                   -c /workspace/yocto/nqma/fli-nqma-yocto-build
-    tmux split-window -h                       -c /workspace/yocto/nqma/fli-nqma-yocto-build
-    tmux split-window -v                       -c /workspace/yocto/nqma/fli-nqma-yocto-build/layers/meta-fli
-    tmux selectp -t 0
-    tmux split-window -v                       -c /workspace/yocto/nqma/fli-nqma-yocto-build/layers/meta-fli
-    # 4
-    tmux new-window -n fli-utils               -c $NQMA_ROOT/fli-utils
-    tmux split-window -h                       -c $NQMA_ROOT/fli-utils
-    tmux selectp -t 0
-    tmux split-window -v                       -c $NQMA_ROOT/fli-utils
-    # # 5
-    # tmux new-window -n   nqma-node-service     -c /workspace/rust/nqma-node-service
-    # tmux split-window -h                       -c /workspace/rust/nqma-node-service
-    # tmux selectp -t 0
-    # tmux split-window -v                       -c /workspace/rust/nqma-node-service
-    # 5
-    tmux new-window -n nqma-fw-bin             -c $NQMA_ROOT/fli-nqma-firmware-bin
-    tmux split-window -h                       -c $NQMA_ROOT/fli-nqma-firmware-bin
-    # 6
-    tmux new-window -n nqma-provision          -c $NQMA_ROOT/fli-utils/scripts
-    tmux split-window -h                       -c $NQMA_ROOT/fli-utils/scripts
-    # 7
-    #   - nqma-sbc-base, base-station
-    tmux new-window -n nqma-sbc-base           -c ~/
-    tmux split-window -h                       -c ~/
-    tmux split-window -v                       -c ~/
-    tmux selectp -t 0
-    tmux split-window -v                       -c ~/
-    # 8
-    # run: setup_scalar_gui to setup python virtual env
-    # to launch the GUI run: DISPLAY=:0.0 python app/app.py &
-    tmux new-window -n 'FLI Recorder'          -c $NQMA_ROOT/scalar-gui
-    tmux split-window -h                       -c $NQMA_ROOT/scalar-gui
-    # 9
-    #   fli-utils-auto - for automated fli-utils running of nqma_pfn_simple_loop.sh/py scripts
-    tmux new-window -n fli-utils-auto          -c $NQMA_ROOT/fli-utils-auto
-    tmux split-window -h                       -c $NQMA_ROOT/fli-utils-auto
-    tmux split-window -v                       -c $NQMA_ROOT/fli-utils-auto
-    tmux selectp -t 0
-    tmux split-window -v                       -c $NQMA_ROOT/fli-utils-auto
-    # 10
-    tmux new-window -n 'logs'                  -c /workspace/logs/nqma
-    tmux split-window -h                       -c /workspace/logs/nqma
-  elif [[ "$SESSION_NAME" == "flm" ]]; then
+  if [[ "$SESSION_NAME" == "flm" ]]; then
     # 1
     tmux new-window -n hs-utils                -c $HEDSCAN_ROOT/hedscan-utils
     tmux split-window -h                       -c $HEDSCAN_ROOT/hedscan-utils
@@ -122,7 +61,7 @@ if [ $ret -eq 1 ]; then
     tmux send-keys -t flm:5.0 "source $PYTHON_VENV_PATH" C-m
     tmux send-keys -t flm:5.1 "source $PYTHON_VENV_PATH" C-m
     tmux send-keys -t flm:5.2 "source $PYTHON_VENV_PATH" C-m
-    # 6
+    # 6 - hedscan-sensor-tune
     tmux new-window -n  hs-sensor-tune         -c $HEDSCAN_ROOT/hedscan-sensor-tune
     tmux split-window -h                       -c $HEDSCAN_ROOT/hedscan-sensor-tune
     tmux selectp -t 0
@@ -130,15 +69,11 @@ if [ $ret -eq 1 ]; then
     tmux send-keys -t flm:6.0 "source $PYTHON_VENV_PATH" C-m
     tmux send-keys -t flm:6.1 "source $PYTHON_VENV_PATH" C-m
     tmux send-keys -t flm:6.2 "source $PYTHON_VENV_PATH" C-m
-    # # 7
-    # tmux new-window -n hs-gui                  -c $HEDSCAN_ROOT/hedscan-gui
-    # tmux split-window -h                       -c $HEDSCAN_ROOT/hedscan-gui
-
-    # 7 - hedscan-docker-qt
-    tmux new-window -n hs-docker-qt            -c $HEDSCAN_ROOT/hedscan-docker-qt
-    tmux split-window -h                       -c $HEDSCAN_ROOT/hedscan-docker-qt
+    # 7 - hedscan-core
+    tmux new-window -n hs-core                 -c $HEDSCAN_ROOT/hedscan-core
+    tmux split-window -h                       -c $HEDSCAN_ROOT/hedscan-core
     tmux selectp -t 0
-    tmux split-window -v                       -c $HEDSCAN_ROOT/hedscan-docker-qt
+    tmux split-window -v                       -c $HEDSCAN_ROOT/hedscan-core
     # 8 - hedscan-api
     tmux new-window -n hs-api                  -c $HEDSCAN_ROOT/hedscan-api
     tmux split-window -h                       -c $HEDSCAN_ROOT/hedscan-api
@@ -151,11 +86,11 @@ if [ $ret -eq 1 ]; then
     tmux selectp -t 0
     tmux split-window -v                       -c /workspace/hedscan/ptc-firmware
     # 9
-    tmux new-window -n hs-yocto                -c /workspace/yocto/hedscan/hedscan-yocto-build
-    tmux split-window -h                       -c /workspace/yocto/hedscan/hedscan-yocto-build
-    tmux split-window -v                       -c /workspace/yocto/hedscan/hedscan-yocto-build/layers/meta-hedscan
+    tmux new-window -n hs-yocto                -c $HEDSCAN_ROOT/hedscan-yocto-build
+    tmux split-window -h                       -c $HEDSCAN_ROOT/hedscan-yocto-build
+    tmux split-window -v                       -c $HEDSCAN_ROOT/hedscan-yocto-build/layers/meta-hedscan
     tmux selectp -t 0
-    tmux split-window -v                       -c /workspace/yocto/hedscan/hedscan-yocto-build
+    tmux split-window -v                       -c $HEDSCAN_ROOT/hedscan-yocto-build
     # 10
     tmux new-window -n hs-mliss                -c $HEDSCAN_ROOT/hedscan-server
     tmux split-window -h                       -c $HEDSCAN_ROOT/hedscan-server
